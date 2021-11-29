1e5ce402ae410b3c395235f698a2a1f37fb49785
RitaBrown <90474717+Cupcakeye@users.noreply.github.com>
2021-11-29 14:28:38 -0500

    v0.1

--- myFirstPyGame.py
+++ myFirstPyGame.py
@@ -1,1 +1,18 @@
 #My Firat PyGame, Tarita Brown, 11/29/21 
+
+import pygame, sys 
+from pygame.locals import *
+
+# start Pygame
+pygame.init()
+
+# Setup our window.
+windowSurface = pygame.display.set_mode((500,400), 0,32)
+pygame.display.set_caption('Hello, world!')
+
+# Setup Colors
+BLACK = (0, 0, 0,)
+WHITE = (255, 255, 255)
+RED = (255, 0,0) 
+GREEN = (0, 255, 0) 
+BLUE = ( 0, 0, 255)
