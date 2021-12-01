3e9206458504f0bce7c539f35d7cc44cd2e0f6da
RitaBrown <90474717+Cupcakeye@users.noreply.github.com>
2021-12-01 14:00:56 -0500

    v0.6

--- myFirstPyGame.py
+++ myFirstPyGame.py
@@ -1,4 +1,4 @@
-#My Firat PyGame, Tarita Brown, 12/1/21 1:55 PM
+#My Firat PyGame, Tarita Brown, 12/1/21 2:00 PM
 
 import pygame, sys 
 from pygame.locals import *
@@ -37,3 +37,6 @@ pygame.draw.polygon(windowSurface, GREEN, ((146,0), (291, 106), (236, 277), (56,
 pygame.draw.line(windowSurface, BLUE, (60,60), (120, 60), 4)
 pygame.draw.line(windowSurface, WHITE, (75,60), (60, 75), 2)
 pygame.draw.line(windowSurface, RED, (0,150), (150, 0), 1)
+
+# Draw a circle.
+pygame.draw.circle(windowSuface, BLACK, (300, 50), 20,0)
