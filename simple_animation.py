df6993a778ceba857d2adc1cd6c8ae0352ae5a5c
RitaBrown <90474717+Cupcakeye@users.noreply.github.com>
2022-01-05 14:30:36 -0500

    v0.2

--- simple_animation.py
+++ simple_animation.py
@@ -1,4 +1,4 @@
-# Simple Animation with PyGame, Tarita Brown, 12/09/21, 2:43PM, v0.1
+# Simple Animation with PyGame, Tarita Brown, 1/05/22, 2:43PM, v0.1
 
 import pygame, sys, time 
 from pygame.locals import *
@@ -11,3 +11,11 @@ WINDOWWIDTH = 400
 WINDOWHEIGHT = 400
 windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
 pygame.display.set_caption('Animation Example!')
+
+# Setup the direction variables.
+DOWNLEFT = 'downleft'
+DOWNRIGHT = 'downright'
+UPLEFT = 'upleft'
+UPRIGHT = 'upright'
+
+MOVESPEED = 4 
