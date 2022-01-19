99678e66bdf3463e296623895420aff5b76fe753
RitaBrown <90474717+Cupcakeye@users.noreply.github.com>
2022-01-19 13:48:32 -0500

    v0.1

---
+++ .github/collision_detection.py
@@ -0,0 +1,8 @@
+# Pygame Collision Detcction Practice, Tarita Brown, Jan 19, 2022, 1:47PM, v0.1
+
+import pygame, sys, random
+from pygame.locals import *
+
+# Setup Pygame
+pygame.init()
+mainClock = pygame.time.Clock()
