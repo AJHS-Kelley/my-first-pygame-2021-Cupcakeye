730651af1f54d7799e72b82661d551785a9b690f
RitaBrown <90474717+Cupcakeye@users.noreply.github.com>
2022-01-19 14:45:33 -0500

    v0.7

--- .github/collision_detection.py
+++ .github/collision_detection.py
@@ -1,4 +1,4 @@
-# Pygame Collision Detcction Practice, Tarita Brown, Jan 19, 2022, 2:22PM, v0.6
+# Pygame Collision Detcction Practice, Tarita Brown, Jan 19, 2022, 2:45PM, v0.7
 
 import pygame, sys, random
 from pygame.locals import *
@@ -39,7 +39,7 @@ MOVESPEED = 6
 # Run the game loop
 while True:
     # Check for events.
-    for event in pygame.event.get()
+    for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
@@ -56,4 +56,20 @@ while True:
                 moveUp = True
             if event.key == K_DOWN or event.key == K_s:
                 moveUp= False
-                moveDown = True               
+                moveDown = True
+        if event.type == KEYUP:
+             if event.key == K_ESCAPE:
+                 pygame.quit()
+                 sys.exit()
+            # Check to see if the player has stopped moving.
+        if event.key == K_LEFT or event.key == K_a:
+                moveLeft = False
+        if event.key == K_RIGHT or event.key == K_d:
+                moveRight = False
+        if event.key == K_UP or event.key == K_w:
+                moveUp = False
+        if event.key == K_DOWN or event.key == K_s:
+            moveDown = False
+        if event.key == K_x: # Use x to teleport the player.
+            player.top = random.randint(0, WINDOWHEIGHT - player.height)
+            player.left = random.randint(0, WINDOWWIDTH - player.width)    
