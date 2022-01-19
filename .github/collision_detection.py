1c76f27b415cc75c98505e99625acc4f1edb923e
RitaBrown <90474717+Cupcakeye@users.noreply.github.com>
2022-01-19 14:10:56 -0500

    v0.5

--- .github/collision_detection.py
+++ .github/collision_detection.py
@@ -1,4 +1,4 @@
-# Pygame Collision Detcction Practice, Tarita Brown, Jan 19, 2022, 1:58PM, v0.3
+# Pygame Collision Detcction Practice, Tarita Brown, Jan 19, 2022, 2:10PM, v0.5
 
 import pygame, sys, random
 from pygame.locals import *
@@ -18,3 +18,20 @@ BLACK = (0, 0, 0)
 GREEN = (0, 225, 0)
 WHITE = (225, 255, 255)
 
+# Setup the player and food date structures.
+foodCounter = 0
+NEWFOOD = 40
+FOODSIZE = 20
+player = pygame.Rect(300, 100, 50, 50)
+foods = []
+
+for i in range(20):
+    foods.append(pygame.Rect(random.randint(0, WINDOWWIDTH - FOODSIZE), random.randint(0, WINDOWHEIGHT - FOODSIZE), FOODSIZE, FOODSIZE))
+
+# Movement Variables
+moveLeft = False
+moveRight = False
+moveUp = False
+moveDown = False 
+
+MOVESPEED = 6
